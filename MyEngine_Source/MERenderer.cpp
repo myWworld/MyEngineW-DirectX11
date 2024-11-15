f99b95198a5c459a3eee483d4a10c6afd5e416ec
lsh552900@naver.com <lsh552900@naver.com>
2024-11-15 21:21:00 +0900

    삼각형 그리기

--- MyEngine_Source/MERenderer.cpp
+++ MyEngine_Source/MERenderer.cpp
@@ -5,4 +5,12 @@ namespace ME::renderer
 {
 	Camera* mainCamera = nullptr;
 	Camera* playerCamera = nullptr;
+
+	Vertex vertexes[3] = {};
+	ID3D11Buffer* vertexBuffer = nullptr;
+	ID3DBlob* vsBlob = nullptr;
+	ID3D11VertexShader* vsShader = nullptr;
+	ID3DBlob* psBlob = nullptr;
+	ID3D11PixelShader* psShader = nullptr;
+	ID3D11InputLayout* inputLayouts = nullptr;
 }
--- MyEngine_Source/MERenderer.h
+++ MyEngine_Source/MERenderer.h
@@ -1,10 +1,25 @@
 #pragma once
 #include "MECamera.h"
+#include "MEGraphicDevice_DX11.h"
 
 namespace ME::renderer 
 {
 	extern Camera* mainCamera;
 	extern Camera* playerCamera;
+
+	struct Vertex
+	{
+		Vector3 pos;
+		Vector4 color;
+	};
+
+	extern Vertex vertexes[3];
+	extern ID3D11Buffer* vertexBuffer;
+	extern ID3DBlob* vsBlob;
+	extern ID3D11VertexShader* vsShader;
+	extern ID3DBlob* psBlob;
+	extern ID3D11PixelShader* psShader;
+	extern ID3D11InputLayout* inputLayouts;
 
 }
 
